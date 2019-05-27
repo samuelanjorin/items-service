85089cf213e60fbb53d0d6018800c2a2bc3c1bbe
Samuel Anjorin <samuel.anjorin12@gmail.com>
2019-05-27 17:39:12 +0100

    updated index

--- constants/index.js
+++ constants/index.js
@@ -1,12 +1,5 @@
 const ERROR_CODES = Object.freeze({
-  USR_01: 'Email or Password is invalid.',
-  USR_02: 'The field(s) are/is required.',
-  USR_03: 'The email is invalid.',
-  USR_04: 'The email already exists.',
-  USR_05: "The email doesn't exist.",
-  USR_06: 'This is an invalid phone number.',
-  USR_07: 'This is too long <FIELD NAME>.',
-  USR_08: 'This is an invalid Credit Card.',
+
   USR_09: 'The Shipping Region ID is not number.',
   AUT_01: 'Authorization code is empty.',
   AUT_02: 'Access Unauthorized.',
